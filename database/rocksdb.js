ad068d1b2ac597324ea8c7dc8f64f9ef00176b3e
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-03-06 06:55:18 -0300

    Abrir o banco de dados

--- database/rocksdb.js
+++ database/rocksdb.js
@@ -6,3 +6,12 @@ const dbPath = path.join(__dirname, "voluntariado.db");
 
 //Inicializa o banco de dados 
 const db = rocksdb(dbPath);
+
+//Abre o banco de dados
+db.open((err) => {
+    if (err) {
+        console.error("Erro ao abrir o banco de dados", err);
+        return;
+    }
+    console.log("Banco de dados aberto");
+});
