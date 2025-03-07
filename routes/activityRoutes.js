2e3641317fc90b5b7c105eaf88239dd2721d9da3
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-03-07 03:38:02 -0300

    Atualizar rotas

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const activityController = require('../controllers/activityController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const activityController = require('../controllers/activityController');
 
-router.post('/', authMiddleware, activityController.createActivity);
-router.get('/', activityController.getAllActivities);
+// Rotas de atividades
+router.post('/', authMiddleware, activityController.createActivity); // Criar atividade (Administrador)
+router.get('/', authMiddleware, activityController.getAllActivities); // Listar atividades
+router.post('/:activityId/register', authMiddleware, activityController.registerUserForActivity); // Inscrever usuário
+router.post('/:activityId/cancel', authMiddleware, activityController.cancelUserRegistration); // Cancelar inscrição
 
 module.exports = router;
