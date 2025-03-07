5a6f456a90a9a624f6943b54de47501f1fae2665
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-03-07 04:01:28 -0300

    Atualizar rotas

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 const activityController = require('../controllers/activityController');
 
 // Rotas de atividades
-router.post('/', authMiddleware, activityController.createActivity); // Criar atividade (Administrador)
-router.get('/', authMiddleware, activityController.getAllActivities); // Listar atividades
-router.post('/:activityId/register', authMiddleware, activityController.registerUserForActivity); // Inscrever usuário
-router.post('/:activityId/cancel', authMiddleware, activityController.cancelUserRegistration); // Cancelar inscrição
+router.post('/', authMiddleware, activityController.createActivity); // Criar atividade (Admin)
+router.get('/', activityController.getAllActivities); // Listar atividades
+router.post('/:activityId/register', authMiddleware, activityController.registerInActivity); // Inscrever usuário
+router.delete('/:activityId/register', authMiddleware, activityController.cancelRegistration); // Cancelar inscrição
+router.put("/:activityId", authMiddleware, activityController.editActivity); // Editar atividade (Admin)
+router.delete("/:activityId", authMiddleware, activityController.deleteActivity); // Remover atividade (Admin)
 
 module.exports = router;
